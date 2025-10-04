d7bb74c615235554fc973124c52dc746a1fb7908
DataHub2 <zakariaallmaninfo@gmail.com>
2025-10-04 14:16:32 +0200

    Börjat rensa dna data

--- Lab1.py
+++ Lab1.py
@@ -4,17 +4,18 @@ import matplotlib
 # I have named the file, and then assigend (dna_raw.txt) as the value. 
 file_name = "dna_raw.txt"
 dic_seq = {}
-key = []
-# This code will open the file that i will be working with (dna_raw.txt)
+id = ""
+# This code will open the file that i will be working with (dna_raw.txt) 
+#W3schools hjälpte mig gå genom detta! Sedan fick jag lite vägledning från Anton 
 with open(file_name) as f:
  for line in f:
-    line = line.strip()
+    line = line.strip().upper()
     if line.startswith(">"):
-        key = line[1:]
-        dic_seq[key] = []
+        id = line[1:]
+        dic_seq[id] = []
     else:
-        dic_seq[key].append(line)
+        dic_seq[id].append(line)
 
-
-        
 print(dic_seq)
+
+clean_seq = {}
