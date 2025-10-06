56e0f178c6f4644b77e28431c5134874f79f8a3c
DataHub2 <zakariaallmaninfo@gmail.com>
2025-10-06 14:33:45 +0200

    jag försöker göra diagram

--- Lab1.py
+++ Lab1.py
@@ -1,12 +1,12 @@
-import matplotlib 
+import matplotlib.pyplot as plt 
 
 # Lab-1 
-# I have named the file, and then assigend (dna_raw.txt) as the value. 
+# jag la in filen i variabel  
 file_name = "dna_raw.txt"
 dic_seq = {}
-id = ""
-# This code will open the file that i will be working with (dna_raw.txt) 
-#W3schools hjälpte mig gå genom detta! Sedan fick jag lite vägledning från Anton 
+id = "" #Jag testa först med [] men det blev fel så jag la in tom citat tecken vilket funkade
+# koden ska vara för att öppna filen  
+                                        #W3schools hjälpte mig gå genom detta! Sedan fick jag lite vägledning från Anton 
 with open(file_name) as f:
  for line in f:
     line = line.strip().upper()
@@ -18,20 +18,19 @@ with open(file_name) as f:
 
 print(dic_seq)
 
-#This code will be counting the letters
+# koden kommer räkna antal bostäver # fick lite vägledning från LLM då jag fastnade helt
 letter_count = {}
 
 for seq_name, dna_string in dic_seq.items():
    count_dic = {"A": 0, "T": 0, "C": 0, "G": 0}
    for letter in dna_string:
-      if letter in count_dic:
+      if letter in count_dic:   
          count_dic[letter] += 1
          letter_count[seq_name] = count_dic
-
- 
-
 
 
 print("letter count")
 print(letter_count) 
+
+
          
